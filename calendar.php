c463934fbadc1f449d7c938467250b9899c433fd
Gersain <gersigner@hotmail.com>
2016-09-30 17:08:53 -0500

    fin  30

---
+++ calendar.php
@@ -0,0 +1,108 @@
+<?php include('header.php'); ?>
+
+ <section class="headerIn" style="background: url('assets/images/dmd-bg.jpg') center;">
+ 	<div class="contentSmall">
+ 	<h2 class="white">TITLE</h2>
+ 	</div>
+ </section>
+
+    <!-- Page Content -->
+    <div class="containerIn container">
+
+        <!-- Marketing Icons Section -->
+        <div class="row">
+            <div class="col-lg-12 paddingAllB">
+                <h1 class="line">
+                   Calendario de eventos 
+                </h1>
+            </div>
+             <div class="col-sm-12 headMainorMed">
+             	<div class="content">
+                  <iframe src="https://www.youtube.com/embed/HflLxXGPUAs" frameborder="0" allowfullscreen></iframe> 
+                  <div class="textHead">
+                  <strong>Vive la Experiencia DMD</strong>     en Cinépolis y descubre las novedades que 
+                  SOLIDWORKS tiene para ti. Conocerás a fondo las funcionalidades, desempeño, e
+                  xperiencias y tips por parte de los expertos para aumentar tu competitividad y detonar la
+                   innovación en tus diseños. <br> <br>
+                    <ul class="general">
+                            <li>
+                                <i class="flaticon-arrows red"></i>
+                                <p>Generación de operaciones adicionales al modelo desde 
+                                    SOLIDWORKS sin perder estos cambios en caso de 
+                                    modificación del modelo original en su plataforma. </p>
+                            </li>
+                            <li>
+                                <i class="flaticon-arrows red"></i>
+                                <p>Mejora de la herramienta envolver</p>
+                            </li>
+                            <li>
+                                <i class="flaticon-arrows red"></i>
+                                <p>Mejora de la herramienta envolver</p>
+                            </li>
+                            <li>
+                                <i class="flaticon-arrows red"></i>
+                                <p>Mejora de la herramienta envolver</p>
+                            </li>
+                            <li>
+                                <i class="flaticon-arrows red"></i>
+                                <p>Mejora de la herramienta envolver</p>
+                            </li>
+                        </ul>
+                  </div>
+                </div>  
+             </div>
+             <hr>
+      
+        </div>
+
+
+        <div class="row">
+            
+
+            <div class="month">
+                  <ul>
+        
+                    <li>
+                      Octubre
+                    </li>
+                  </ul>
+                </div>
+
+                <ul class="weekdays">
+                  <li>L</li>
+                  <li>M</li>
+                  <li>M</li>
+                  <li>J</li>
+                  <li>V</li>
+                  <li>S</li>
+                  <li>D</li>
+                </ul>
+
+                <ul class="days">
+                  <li>1</li>
+                  <li>2</li>
+                  <li>3</li>
+                  <li>4</li>
+                  <li>5</li>
+                  <li>6</li>
+                  <li>7</li>
+                  <li>8</li>
+                  <li>9</li>
+                  <li><span class="active">10</span></li>
+                  <li>11</li>
+                  ...etc
+                </ul>
+        </div>
+
+      </div> 
+ 
+   <!-- Large modal -->
+<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      ...
+    </div>
+  </div>
+</div>   
+
+<?php include('footer.php'); ?>
