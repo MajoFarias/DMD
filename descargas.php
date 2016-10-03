bd71464a622cff4407a826328c46fabe111cbcd2
Majo Farías <majographic@gmail.com>
2016-10-03 16:36:35 -0500

    Cambios Finales

---
+++ descargas.php
@@ -0,0 +1,57 @@
+<?php include('header.php'); ?>
+
+ <section class="headerIn" style="background: url('assets/images/eyec2.jpg') center;">
+ 	<div class="contentSmall">
+ 	<h2 class="white">SOLIDWORKS® 2017</h2>
+ 	</div>
+ </section>
+
+    <!-- Page Content -->
+    <div class="containerIn container">
+
+        <!-- Marketing Icons Section -->
+        <div class="row">
+            <div class="col-lg-12 paddingAllB">
+                <h1 class="line">
+                  Descargas
+                </h1>
+            </div>   
+        </div>
+
+        <div class="row descargas">
+          <div class="col-xx col-xs-6 col-sm-3">
+           <h2><span>Soluciones</span><br>
+            SolidWorks</h2>
+            <div class="divdescarga">
+              <img src="assets/images/pdf.png" alt="">
+              <p><a href="#">Descarga PDF</a></p>
+            </div>
+          </div>
+          <div class="col-xx col-xs-6 col-sm-3">
+           <h2><span>Soluciones</span><br>
+            SolidWorks</h2>
+            <div class="divdescarga">
+              <img src="assets/images/pdf.png" alt="">
+              <p><a href="#">Descarga PDF</a></p>
+            </div>
+          </div>
+          <div class="col-xx col-xs-6 col-sm-3">
+           <h2><span>Soluciones</span><br>
+            SolidWorks</h2>
+            <div class="divdescarga">
+              <img src="assets/images/pdf.png" alt="">
+              <p><a href="#">Descarga PDF</a></p>
+            </div>
+          </div>
+          <div class="col-xx col-xs-6 col-sm-3">
+           <h2><span>Soluciones</span><br>
+            SolidWorks</h2>
+            <div class="divdescarga">
+              <img src="assets/images/pdf.png" alt="">
+              <p><a href="#">Descarga PDF</a></p>
+            </div>
+          </div>
+        </div>
+    </div>
+
+<?php include('footer.php'); ?>
