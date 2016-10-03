4f3a49e59c88803b85b05936b6cc59b0c9d86f0e
Gersain <gersigner@hotmail.com>
2016-10-03 14:46:55 -0500

    integra ger

---
+++ assets/js/tool.js
@@ -0,0 +1,16 @@
+
+    //<![CDATA[
+    $(function(){
+      $('#demo-basic').poshytip();
+      $('#demo-tip-yellow').poshytip();
+       $('#demo-tip-yellow2').poshytip();
+        $('#demo-tip-yellow3').poshytip();
+          $('#demo-tip-yellow4').poshytip();
+          $('#demo-tip-yellow5').poshytip();
+      $('#demo-tip-violet').poshytip({
+        className: 'tip-violet',
+        bgImageFrameSize: 9
+      });
+   
+    });
+    //]]>
